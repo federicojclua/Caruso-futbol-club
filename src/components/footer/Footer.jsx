ae4879ee5a7472cecc1c5de44691203af608788c
Herrera-Pablo <pablogastonherrerat@gmail.com>
2024-05-24 16:39:54 -0300

    Actualización del footer: Cambio de estilos y agregado de lista de enlaces. Se aplicaron los colores especificados en el footer. Se eliminaron los botones de contacto y nosotros, reemplazándolos por una lista de enlaces que incluye contacto, dirección y quiénes somos. Se ajustaron los estilos para garantizar un buen contraste y una apariencia moderna del footer.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,35 +6,36 @@ import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg
 
 const Footer = () => {
   return (
-   <>
-   
-    <footer className="footer bg-dark text-white py-4">
+    <footer className="footer">
       <Container>
-        <Row>
+        <Row className="align-items-center">
           <Col xs={12} md={4} className="text-center text-md-left mb-3 mb-md-0">
             <img src="src/assets/img/cfc-logo.png" alt="Logo Caruso" className="footer-logo" />
           </Col>
-          <Col xs={12} md={4} className="text-center mb-3 mb-md-0">
-            <Button variant="primary" href="/contact">Contáctanos</Button>
+          <Col xs={12} md={4} className="text-center text-md-left mb-3 mb-md-0">
+            <ul className="footer-links">
+              <li><a href="/contact">Contacto</a></li>
+              <li><a href="/address">Dirección</a></li>
+              <li><a href="/about">Quiénes Somos</a></li>
+            </ul>
           </Col>
           <Col xs={12} md={4} className="text-center text-md-right">
-            <a href="https://facebook.com" className="text-white mx-2">
-              <FontAwesomeIcon icon={faFacebook} size="2x" />
-            </a>
-            <a href="https://twitter.com" className="text-white mx-2">
-              <FontAwesomeIcon icon={faTwitter} size="2x" />
-            </a>
-            <a href="https://instagram.com" className="text-white mx-2">
-              <FontAwesomeIcon icon={faInstagram} size="2x" />
-            </a>
-            <div className="mt-2">
-            <Button variant="primary" href="/contact">Nosotros</Button>
+            <div className="social-icons">
+              <Button variant="link" className="mx-2" href="https://facebook.com">
+                <FontAwesomeIcon icon={faFacebook} size="2x" />
+              </Button>
+              <Button variant="link" className="mx-2" href="https://twitter.com">
+                <FontAwesomeIcon icon={faTwitter} size="2x" />
+              </Button>
+              <Button variant="link" className="mx-2" href="https://instagram.com">
+                <FontAwesomeIcon icon={faInstagram} size="2x" />
+              </Button>
             </div>
           </Col>
         </Row>
       </Container>
     </footer>
-   </>
   );
 }
+
 export default Footer;
