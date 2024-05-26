0c8eee241989b320df9b733fcce93731c75be033
Maru <mariadelcarmenmarcoux@gmail.com>
2024-05-26 02:10:45 -0300

    se agrego carrousel y lista desplegale

--- src/components/Galeria-productos/Galeria.jsx
+++ src/components/Galeria-productos/Galeria.jsx
@@ -1,53 +1,50 @@
 import React from 'react';
 import "./Galeria.css";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Placeholder from 'react-bootstrap/Placeholder';
-
+import Carousel from 'react-bootstrap/Carousel';
 
 const Galeria = () => {
   return (
-   <>
-   <div className="d-flex justify-content-around">
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="src\assets\img\canchas.jpg" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-         
-        </Card.Body>
-      </Card>
-
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="src\assets\img\cantina2.jpg" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-         
-        </Card.Body>
-      </Card>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="src\assets\img\market.png" />
-        <Card.Body>
-          <Card.Title>Caruso shop</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Mi Tienda</Button>
-        </Card.Body>
-      </Card>
-
+    <div className="gallery">
+      {/* Contenedor para el Carousel */}
+      <div className="carousel-container">
+        <Carousel>
+          <Carousel.Item interval={1000}>
+            <img
+              className="d-block w-100"
+              src="src\assets\img\canchas.jpg"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={500}>
+            <img
+              className="d-block w-100"
+              src="src\assets\img\cantina2.jpg"
+              alt="Second slide"
+            />
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="src\assets\img\market.png"
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
     </div>
-   
- 
-   </>
   );
 }
+
 export default Galeria;
--- src/components/header/reservacion/Reservacion.jsx
+++ src/components/header/reservacion/Reservacion.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap'; // Eliminamos la importación de Dropdown aquí
 import "./Reservacion.css";
+import List from './list/List';
 
 const Reservacion = () => {
   return (
-   <>
-   
-   
-   <div>
-        <h1>Reservacion</h1>
+    <>
+      <div className="reservation-container">
+        <div className="reservation-button">
+          <h2>Hace tu reservación</h2>
+        </div>
+      
       </div>
-   
-   </>
+      <List/>
+
+    </>
   );
 }
+
 export default Reservacion;
