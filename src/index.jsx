b166a23af9c0f7feaa154ec350487f25e43f8ced
Maru <mariadelcarmenmarcoux@gmail.com>
2024-05-25 21:03:34 -0300

    se creo seccion sponsor

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import Escuelita from './components/escuelita-futbol/Escuelita';
 import Footer from './components/footer/Footer';
 import Galeria from './components/Galeria-productos/Galeria';
 import Header from './components/header/Header';
+import Sponsors from './components/Sponsors/Sponsors';
 import InfoTorneos from './components/Info-torneos/InfoTorneos';
 import NavBar from './components/header/nav-bar/NavBar';
 import WhatsAppButton from './components/WhatsAppButton/WhatsAppButton';
