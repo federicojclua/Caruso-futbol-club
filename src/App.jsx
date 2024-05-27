0d17dba5d3aed75fd61b1b91c8b052b7d1f0125c
Maru <mariadelcarmenmarcoux@gmail.com>
2024-05-27 00:35:57 -0300

    correcion app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import './App.css'
 import Escuelita from './components/escuelita-futbol/Escuelita';
 import Footer from './components/footer/Footer';
 import Galeria from './components/Galeria-productos/Galeria';
 import Header from './components/header/Header';
-import InfoTorneos from './components/Info-torneos/InfoTorneos';
 import Sponsors from './components/Sponsors/Sponsors';
+import InfoTorneos from './components/Info-torneos/InfoTorneos';
 import NavBar from './components/header/nav-bar/NavBar';
 import WhatsAppButton from './components/WhatsAppButton/WhatsAppButton';
-
-
 function App() {
+
+
   return (
-    <Router>
-      <>
-        <Header />
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Galeria />} />
-          <Route path="/escuelita" element={<Escuelita />}/>
-          <Route path="/info-torneos" element={<InfoTorneos/>} />
-          </Routes>
-        <Footer />
-        <WhatsAppButton />
-      </>
-    </Router>
-  );
+    <>
+    
+      <Header/>
+      <Galeria/>
+      <Escuelita/>
+      <InfoTorneos/>
+      <Sponsors/>
+      
+
+      
+      <Footer/>
+      <WhatsAppButton/>
+      
+    </>
+  )
 }
 
-export default App;
-
+export default App
