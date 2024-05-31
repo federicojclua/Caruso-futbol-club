47a3e5a8a88a8fa87f873672afe480600b3f83a9
Maru <mariadelcarmenmarcoux@gmail.com>
2024-05-31 18:43:39 -0300

    se borro componente list

--- src/components/header/reservation/Reservation.jsx
+++ src/components/header/reservation/Reservation.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap'; // Eliminamos la importación de Dropdown aquí
 import "./Reservation.css";
-import List from './List/List'
+
 
 const Reservation = () => {
   return (
     <>
-      <container className="reservation-container">
+      <Container className="reservation-container">
         <div className="reservation-button">
-          <h2>Hace tu reservación</h2>
+          <h2>Hace tu reservación el día de</h2>
         </div>
       
-      </container>
-      <List/>
+      </Container>
+      
 
     </>
   );
