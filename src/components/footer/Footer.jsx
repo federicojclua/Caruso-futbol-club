157aaff3001efcb6696ef78de19b817140ea55d5
Herrera-Pablo <pablogastonherrerat@gmail.com>
2024-06-01 18:05:03 -0300

    nuevos detalles del footer, creación carpetas y pages de contacto y acerca de nosotros, falta añadir el contenido de las pages

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,66 +1,53 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import "./Footer.css";
-import { Container, Row, Col, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faInstagram, faTwitter, faTiktok } from '@fortawesome/free-brands-svg-icons';
-
 
 const Footer = () => {
   return (
     <footer className="footer">
-        <div class="footer-container">
-            <div class="footer-column">
-            <img src="src/assets/img/cfc-logo.png" alt="Logo Caruso" className="footer-logo" />
-<<<<<<< HEAD
-            </div>
-            <div class="footer-column">
-                <h3>Información</h3>
-                <p>Algunas líneas sobre la página web y su propósito.</p>
-            </div>
-            <div class="footer-column">
-                <h3>Enlaces</h3>
-                <ul>
-                    <li><a href="#">Contacto</a></li>
-                    <li><a href="#">Quiénes Somos</a></li>
-                    <li><a href="#">Términos y Condiciones</a></li>
-                    <li><a href="#">Política de Privacidad</a></li>
-                </ul>
-            </div>
-            <div class="footer-column">
-                <h3>Síguenos</h3>
-                <div class="social-icons">
-                <div className="social-icons">     
-=======
-          </Col>
-          <Col xs={12} md={4} className="text-center text-md-left mb-3 mb-md-0">
-            <ul className="footer-links">
-              <li><a href="/contact"><button class="btn-hover color-1">Contacto</button></a></li>
-              <li><a href="/address"><button class="btn-hover color-1">Quiénes somos</button></a></li>
-              <li><a href="/about"><button class="btn-hover color-1">Dirección</button></a></li>
-              
-            </ul>
-          </Col>
-          <Col xs={12} md={4} className="text-center text-md-right">
-            <div className="social-icons">     
->>>>>>> 9703d2c80a33c651696dc430653071f1e3652f66
-             <div className="rounded-social-buttons">
-        <a className="social-button facebook" href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faFacebook} size="2x" />
-        </a>
-        <a className="social-button twitter" href="https://www.twitter.com/" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faTwitter} size="2x" />
-        </a>
-        <a className="social-button tiktok" href="https://www.tiktok.com/" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faTiktok} size="2x" />
-        </a>
-        <a className="social-button instagram" href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faInstagram} size="2x" />
-        </a>
-      </div>
-            </div>
-                </div>
-            </div>
+      <div className="footer-container">
+        <div className="footer-column">
+          <img src="src/assets/img/cfc-logo.png" alt="Logo Caruso" className="footer-logo" />
         </div>
+        <div className="footer-column">
+          <h5>Información</h5>
+          <p>| Caruso Fútbol Club |<br />
+            ⚽️ Alquiler de canchas de fútbol 5, 6, 7 y 8 <br />
+            Contamos con Parrilla, Buffet, Bebidas y más
+          </p>
+        </div>
+        <div className="footer-column">
+          <h5>Accesos útiles</h5>
+          <ul>
+            <li><a href="#">Contacto</a></li>
+            <li><a href="#">Acerca de nosotros</a></li>
+            <li><a href="#">Donde encontrarnos</a></li>
+            <li><a href="#">Política de Privacidad</a></li>
+          </ul>
+        </div>
+        <div className="footer-column">
+          <h3>Síguenos</h3>
+          <div className="social-icons">
+            <a className="social-button facebook" href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faFacebook} size="2x" />
+            </a>
+            <a className="social-button twitter" href="https://www.twitter.com/" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faTwitter} size="2x" />
+            </a>
+            <a className="social-button tiktok" href="https://www.tiktok.com/" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faTiktok} size="2x" />
+            </a>
+            <a className="social-button instagram" href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faInstagram} size="2x" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>Diseñado por Equipo 3</p>
+        <p>Todos los derechos reservados © 2024</p>
+      </div>
     </footer>
   );
 }
---
+++ src/components/footer/gg.ts
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import "./Footer.css";
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram, faTwitter, faTiktok } from '@fortawesome/free-brands-svg-icons';
+
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={4} className="text-center text-md-left mb-3 mb-md-0">
+            <img src="src/assets/img/cfc-logo.png" alt="Logo Caruso" className="footer-logo" />
+          </Col>
+          <Col xs={12} md={4} className="text-center text-md-left mb-3 mb-md-0">
+            <ul className="footer-links">
+              <li><a href="/contact"><button class="btn-hover color-1">Contacto</button></a></li>
+              <li><a href="/address"><button class="btn-hover color-1">Quienes somos</button></a></li>
+              <li><a href="/about"><button class="btn-hover color-1">Dirección</button></a></li>
+              
+            </ul>
+          </Col>
+          <Col xs={12} md={4} className="text-center text-md-right">
+            <div className="social-icons">     
+             <div className="rounded-social-buttons">
+        <a className="social-button facebook" href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faFacebook} size="2x" />
+        </a>
+        <a className="social-button twitter" href="https://www.twitter.com/" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faTwitter} size="2x" />
+        </a>
+        <a className="social-button tiktok" href="https://www.tiktok.com/" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faTiktok} size="2x" />
+        </a>
+        <a className="social-button instagram" href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faInstagram} size="2x" />
+        </a>
+      </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+}
+
+export default Footer;
