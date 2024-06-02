2e16dda9b54e443b3b45d0f975f3e8916b70adab
Maru <mariadelcarmenmarcoux@gmail.com>
2024-06-02 03:53:36 -0300

    modificacion Sponsors

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import "./Sponsors.css";
+import { Container, Row, Col } from 'react-bootstrap';
 
-const Sponsors= () => {
+const Sponsors = () => {
   return (
-   <>
-   
-   
-   <div className='Sponsors'>
-        <h1>SOMOS LOS SPONSORS</h1>
-      </div>
-   
-   </>
+    <Container className='Sponsors'>
+      <p className='text-sponsors'>Confían en Nosotros</p>
+    <Row>
+      <Col className="d-flex justify-content-center">
+        <img src="src/assets/img/sponsor1.png" alt="Sponsor 1" className="img-fluid w-100" style={{ maxWidth: '200px' }}/>
+      </Col>
+      <Col className="d-flex justify-content-center">
+        <img src="src/assets/img/sponsor1.png" alt="Sponsor 2" className="img-fluid w-100" style={{ maxWidth: '200px' }} />
+      </Col>
+      <Col className="d-flex justify-content-center">
+        <img src="src/assets/img/sponsor1.png" alt="Sponsor 3" className="img-fluid w-100" style={{ maxWidth: '200px' }} />
+      </Col>
+      <Col className="d-flex justify-content-center">
+        <img src="src/assets/img/sponsor1.png" alt="Sponsor 4" className="img-fluid w-100" style={{ maxWidth: '200px' }} />
+      </Col>
+    </Row>
+  </Container>
   );
 }
+
 export default Sponsors;
