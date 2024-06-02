87ae7575e22b07640ce4332455e48ac7f016ed28
Herrera-Pablo <pablogastonherrerat@gmail.com>
2024-06-02 18:22:02 -0300

    se añadieron clases container, se acomodo el codigo y se agrego un fondo similiar al del main a la page contacto

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -4,6 +4,7 @@ import './Contact.css';
 import NavBar from '../../components/header/nav-bar/NavBar';  
 import Footer from '../../components/footer/Footer';
 import Header from '../../components/header/Header';
+import { Container } from 'react-bootstrap';
 
 const Contacto = () => {
   return (
@@ -11,49 +12,54 @@ const Contacto = () => {
       <header className="header">
         <NavBar />
       </header>
-      <main className="main-content">
-        <h1>Contacto</h1>
-        <p>Ponte en contacto con nosotros para cualquier consulta o duda.</p>
+      <Container className="main-content">
+        <h2 className='title-contact'>Contacto</h2>
+        <p className='subtitle-contact'>Ponte en contacto con nosotros para cualquier consulta o duda.</p>
 
+        {/* Sección de Información de Contacto */}
         <section className="form">
           <div className="cont-contact">
-            <div className="informacion">
-            <a className='caller' href="tel:01122500777">011 2250 0777</a>
+            <div className="info">
+              <a className='caller' href="tel:01122500777">011 2250 0777</a>
               <p><strong>Servicio de Atención al Cliente</strong></p>
-              <i className="linea-amarilla"></i>
+              <i className="line-orange"></i>
               <p>¡Dale, armá tu equipo y vení! Podes comunicarte en los siguientes horarios:</p>
-              <ul className="order-center-list">
-                <li className="">
-                  <p>⚽️
-                    <strong>Lunes a viernes: </strong><br />
-                    de 8:30 a 13:30 y 16:30 a 21 hs.
-                  </p>
-                </li>
-                <li className="">
-                  <p>⚽️
-                    <strong>Sábados: </strong><br />
-                    de 9 a 13:30 y 16:30 a 21 hs.
-                  </p>
-                </li>
-                <li className="icon-mail">
-                  <p>⚽️
-                    <strong>Email:</strong><br />
-                    <a style={{ fontSize: '16px', padding: '0px', margin: '0px' }} href="mailto:carusofutbolclub@gmail.com">carusofutbolclub@gmail.com</a>
-                  </p>
-                </li>
-                <li className="">
-                  <p>⚽️
-                    <strong>Whatsapp:</strong><br />
-                    <a style={{ fontSize: '16px', padding: '0px', margin: '0px' }} href="https://wa.me/5491122500777">+5491122500777</a>
-                  </p>
-                </li>
-              </ul>
             </div>
           </div>
         </section>
 
-        <Footer />
-      </main>
+        {/* Lista Desordenada de Contacto */}
+        <section className="contact-list">
+          <ul>
+            <li>
+              <p>⚽️
+                <strong>Lunes a viernes: </strong><br />
+                de 8:30 a 13:30 y 16:30 a 21 hs.
+              </p>
+            </li>
+            <li>
+              <p>⚽️
+                <strong>Sábados: </strong><br />
+                de 9 a 13:30 y 16:30 a 21 hs.
+              </p>
+            </li>
+            <li className="icon-mail">
+              <p>⚽️
+                <strong>Email:</strong><br />
+                <a style={{ fontSize: '16px', padding: '0px', margin: '0px' }} href="mailto:carusofutbolclub@gmail.com">carusofutbolclub@gmail.com</a>
+              </p>
+            </li>
+            <li>
+              <p>⚽️
+                <strong>Whatsapp:</strong><br />
+                <a style={{ fontSize: '16px', padding: '0px', margin: '0px' }} href="https://wa.me/5491122500777">+5491122500777</a>
+              </p>
+            </li>
+          </ul>
+        </section>
+        
+      </Container>
+      <Footer />
     </div>
   );
 }
