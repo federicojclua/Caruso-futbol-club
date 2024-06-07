70270168039e259cad1ca1a48d9775cd895fd3a6
Maru <mariadelcarmenmarcoux@gmail.com>
2024-06-06 22:13:56 -0300

    correcion galeria y header

--- src/components/Galeria-productos/Galeria.jsx
+++ src/components/Galeria-productos/Galeria.jsx
@@ -3,21 +3,14 @@ import "./Galeria.css";
 import Carousel from 'react-bootstrap/Carousel';
 import { Container } from 'react-bootstrap';
 
+
 const Galeria = () => {
   return (
+    
     <Container className="container-gallery">
       {/* Contenedor para el texto */}
       <div className="gallery-text">
         <h1 className="gallery-title">¡Mirá todo lo que tenemos para vos!</h1>
-        
-          {/* <p className="gallery-subtitle">Te Invitamos a Disfrutar   </p>
-            <p classname="gallery-list-text">
-            <li>Nuestras canchas son de césped sintético de alta calidad. </li>
-            <li>Ambiente familiar y seguro, ideal para todas las edades y niveles. </li>
-            <li>Horarios flexibles: Abierto todos los días para que puedas jugar cuando más te convenga.. </li>
-            <li>Eventos y torneos: Participa en nuestras competiciones y demuestra tus habilidades. </li>
-            </p> */}
-
           
       </div>
       {/* Contenedor para el Carousel */}
@@ -26,39 +19,64 @@ const Galeria = () => {
           <Carousel.Item interval={1000}>
             <img
               className="d-block w-100 img-carousel"
-              src="/src/assets/img/canchas_3.jpg"
+              src="src\assets\img\carrousel\entrada.jpg"
               alt="First slide"
             />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item interval={500}>
             <img
               className="d-block w-100 img-carousel"
-              src="/src/assets/img/canchas_3.jpg"
+              src="/src/assets/img/carrousel/canchas_3.jpg"
               alt="Second slide"
             />
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
+
           </Carousel.Item>
           <Carousel.Item>
             <img 
               className="d-block w-100 img-carousel"
-              src="/src/assets/img/canchas_3.jpg"
+              src="/src/assets/img/carrousel/canchas.jpg"
               alt="Third slide"
             />
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img 
+              className="d-block w-100 img-carousel"
+              src="src/assets/img/carrousel/quincho2.jpg"
+              alt="Third slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img 
+              className="d-block w-100 img-carousel"
+              src="src/assets/img/carrousel/quincho.jpg"
+              alt="Third slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img 
+              className="d-block w-100 img-carousel"
+              src="src/assets/img/carrousel/canchas2.jpeg"
+              alt="Third slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img 
+              className="d-block w-100 img-carousel"
+              src="src/assets/img/carrousel/cancha1.jpg"
+              alt="Third slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img 
+              className="d-block w-100 img-carousel"
+              src="src/assets/img/carrousel/cancha_nocturna.jpg"
+              alt="Third slide"
+            />
           </Carousel.Item>
         </Carousel>
       </div>
     </Container>
+    
   );
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,21 +9,12 @@ const Header = () => {
   return (
     <header className="header">
       <NavBar />
-      <Container >
-        <div className="video-container">
-          
-              <video autoPlay loop muted className="background-video">
-                <source src="src/assets/video/header-video2.mp4" type="video/mp4" />
-                Tu navegador no soporta el elemento de video.
-              </video>
-       
-        </div>
+      <Container className="Container-video" >
+        <video autoPlay loop muted className="background-video">
+          <source src="src/assets/video/header-video2.mp4" type="video/mp4" />
+        </video>
       </Container>
-      <Container className="content">
-       
-        <p>Hace tu reservacion </p>
 
-      </Container>
     </header>
   );
 }
