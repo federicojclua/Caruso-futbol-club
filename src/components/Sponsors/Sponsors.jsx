8a8bfb3925f699a5eca73776474d333a00fe66f3
Maru <mariadelcarmenmarcoux@gmail.com>
2024-06-09 15:18:28 -0300

    correcion de errores

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -6,21 +6,23 @@ const Sponsors = () => {
   return (
     <Container className='Sponsors'>
       <p className='text-sponsors'>Confían en Nosotros</p>
-    <Row className="img-sponsors">
-      <Col className="d-flex justify-content-center">
-        <img src="src/assets/img/sponsor1.png" alt="Sponsor 1" className="img-fluid w-100" style={{ maxWidth: '200px' }}/>
-      </Col>
-      <Col className="d-flex justify-content-center">
-        <img src="src/assets/img/sponsor1.png" alt="Sponsor 2" className="img-fluid w-100" style={{ maxWidth: '200px' }} />
-      </Col>
-      <Col className="d-flex justify-content-center">
-        <img src="src/assets/img/sponsor1.png" alt="Sponsor 3" className="img-fluid w-100" style={{ maxWidth: '200px' }} />
-      </Col>
-      <Col className="d-flex justify-content-center">
-        <img src="src/assets/img/sponsor1.png" alt="Sponsor 4" className="img-fluid w-100" style={{ maxWidth: '200px' }} />
-      </Col>
-    </Row>
-  </Container>
+      <div className="overflow-hidden">
+        <Row className="img-sponsors">
+          <Col className="d-flex justify-content-center">
+            <img src="src/assets/img/sponsor1.png" alt="Sponsor 1" className="img-fluid" />
+          </Col>
+          <Col className="d-flex justify-content-center">
+            <img src="src/assets/img/sponsor1.png" alt="Sponsor 2" className="img-fluid" />
+          </Col>
+          <Col className="d-flex justify-content-center">
+            <img src="src/assets/img/sponsor1.png" alt="Sponsor 3" className="img-fluid" />
+          </Col>
+          <Col className="d-flex justify-content-center">
+            <img src="src/assets/img/sponsor1.png" alt="Sponsor 4" className="img-fluid" />
+          </Col>
+        </Row>
+      </div>
+    </Container>
   );
 }
 
