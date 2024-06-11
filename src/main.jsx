85ab27dcce24a85fac11acd5c17b2f8f6f0e015b
Maru <mariadelcarmenmarcoux@gmail.com>
2024-06-11 19:39:01 -0300

    se corrigio main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './App.css'
+import React from 'react';
+import { render } from 'react-dom'; // Importa render desde 'react-dom'
+import App from './App.jsx';
+import './App.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
