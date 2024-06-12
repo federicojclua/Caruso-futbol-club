4c9d8cc31ffdc2fdc9094cc311171da6818301e3
Maru <mariadelcarmenmarcoux@gmail.com>
2024-06-12 13:32:08 -0300

    se corrigio link del video en header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
       <NavBar />
       <Container className="Container-video">
         <video autoPlay loop muted className="background-video">
-          <source src=".src/assets/video/header-video2.mp4" type="video/mp4" />
+          <source src="./src/assets/video/header-video2.mp4" type="video/mp4" />
         </video>
         <div className="wrapper">
           <a href='/Principal'><button className='button-record'>
