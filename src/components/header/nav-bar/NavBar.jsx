35a3a9b99572ad1d315e6a662cbef60d1ce91351
Maru <mariadelcarmenmarcoux@gmail.com>
2024-06-12 18:07:49 -0300

    correcion links imagenes

--- src/components/header/nav-bar/NavBar.jsx
+++ src/components/header/nav-bar/NavBar.jsx
@@ -3,7 +3,8 @@ import { Link, useNavigate } from 'react-router-dom';
 import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import "./NavBar.css";
 import Container from 'react-bootstrap/Container';
-import imgLogonav from './src/assets/img/cfc-logo.png';
+import imgLogonav from '../../../assets/img/carpeta/cfc-logo.png';
+import logoincio from '../../../assets/img/carpeta/cfc-logo.png';
 
 const NavBar = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Mover la propiedad 'base' fuera de 'build'
+  base: '/',  // Ajusta esta ruta según sea necesario
   build: {
     manifest: false,
   },
 });
-
