e73e022dd859548e24dd50b39654626fd6f49321
Maru <mariadelcarmenmarcoux@gmail.com>
2024-06-17 16:54:00 -0300

    correcion de links

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import '../Error404/Error404.css';
 import { Container } from 'react-bootstrap';
 import NavBar from '../../components/header/nav-bar/NavBar';  
-import error404gif from '../../assets/img/carpeta/error404.gif';
+import gif404 from '../../../assets/img/carpeta/error404.gif';
 
 const Error404 = () => {
   return (
@@ -12,7 +12,7 @@ const Error404 = () => {
       <NavBar />
       <Container className="main-content-error">
         <h2 className="animate__animated animate__rotateIn">Error 404</h2>
-        <img className='error404gif'src={error404gif} alt="img-error404" />
+        <img className='error404gif'src={gif404} alt="img-error404" />
         <p><Link to="/" className="link-error animate__animated animate__flash">Volver a la página principal</Link></p>
         <p>Lo sentimos, la página que estás buscando no existe.</p>
         <p>Puede que hayas escrito mal la dirección o que la página se haya pinchado.</p>
