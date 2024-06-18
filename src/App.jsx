092cb732d69715d628a8ffd02d907b2e4e074191
caro3221 <86697084+caro3221@users.noreply.github.com>
2024-06-17 22:39:00 -0300

    se corrige conexión render

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,21 @@ import Tournament from './components/Tournament/Tournament';
 import NavBar from './components/header/nav-bar/NavBar';
 import WhatsAppButton from './components/WhatsAppButton/WhatsAppButton';
 import LocationMain from './components/LocationMain/LocationMain';
+import React, { useContext } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'animate.css';
+import './App.css';
+import Info from './components/Info/Info';
+import Footer from './components/footer/Footer';
+import Gallery from './components/Gallery/Gallery';
+import Header from './components/header/Header';
+import Sponsors from './components/Sponsors/Sponsors';
+import Tournament from './components/Tournament/Tournament';
+import NavBar from './components/header/nav-bar/NavBar';
+import WhatsAppButton from './components/WhatsAppButton/WhatsAppButton';
+import LocationMain from './components/LocationMain/LocationMain';
 import Advertising from './components/Advertising/Advertising';
 import Error404 from './pages/Error404/Error404';
 import Contact from './pages/Contact/Contact';
@@ -25,28 +40,13 @@ import Login from './pages/Login/Login';
 import Admin from './pages/Admin/Admin';
 import Record from './pages/record/Record';
 import Principal from './pages/Principal/Principal';
-<<<<<<< HEAD
-
-const backendUrl = 'https://caruso-prueba-back-1.onrender.com'; // Reemplaza con la URL pública de tu backend
-
-const App = () => {
-  return (
-    <div>
-      <MyComponent backendUrl={backendUrl} />
-    </div>
-  );
-};
-
-export default App;
-
-=======
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 import { AuthProvider } from './components/context/AuthProvider';
 import AdminRoute from './components/ProtectedRoute/AdminRoute';
->>>>>>> beaeedbf9e81f3d13475ed151d211b872cd5f242
+
+const backendUrl = 'https://caruso-prueba-back-1.onrender.com'; // Reemplaza con la URL pública de tu backend
 
 function App() {
-
   return (
     <AuthProvider>
       <BrowserRouter>
