144112e470a5ecc667a57a9522c45f505056afe2
caro3221 <86697084+caro3221@users.noreply.github.com>
2024-06-19 22:17:28 -0300

    prueba render 6

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
             <span></span>
             <span></span>
             <span></span>
-            Reserva
+            prueba
           </button></a>
         </div>
       </Container>
