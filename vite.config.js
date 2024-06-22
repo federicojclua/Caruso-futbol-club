8e4b41cc9453403cc04519fe0b331b8971f6a352
caro3221 <86697084+caro3221@users.noreply.github.com>
2024-06-21 21:21:14 -0300

    se vuleve a mod vite

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
 
-// Cargar las variables de entorno desde el archivo .env en desarrollo local
-if (process.env.NODE_ENV !== 'production') {
-  dotenv.config();
-}
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ajusta esta ruta según sea necesario
+  base: '/',  // Ajusta esta ruta según sea necesario
   build: {
     manifest: false,
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.REACT_APP_API_URL,
-        changeOrigin: true,
-      },
-    },
   },
 });
