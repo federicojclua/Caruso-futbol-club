96a0ca099cd4e45d1b14199878e5f6c04d06a8ba
caro3221 <86697084+caro3221@users.noreply.github.com>
2024-06-25 00:22:06 -0300

    se corrige link de conexión

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: , import.meta.env.REACT_APP_API_URL, 
+  baseURL:  import.meta.env.REACT_APP_API_URL, 
   headers: {
     'Content-Type': 'application/json',
   },
--- src/components/apiComponents/CanchasComponent.jsx
+++ src/components/apiComponents/CanchasComponent.jsx
@@ -1,6 +1,6 @@
 // src/components/CanchasComponent.jsx
 import React, { useEffect, useState } from 'react';
-import { obtenerCanchasDisponibles, crearCancha, actualizarCancha, eliminarCancha } from '../apiComponentes/canchasApi';
+import { obtenerCanchasDisponibles, crearCancha, actualizarCancha, eliminarCancha } from '../../api/canchasApi';
 
 const CanchasComponent = () => {
   const [canchas, setCanchas] = useState([]);
