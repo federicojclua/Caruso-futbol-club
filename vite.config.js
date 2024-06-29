c8b868395e553b1e908dd6051a4fa2c768f37eb7
Maria Marcoux <mariadelcarmenmarcoux@gmail.com>
2024-06-29 20:54:37 -0300

    correcion vite config dotenv

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
 
 
+// Importa SweetAlert2 para asegurarte de que esté disponible
+import 'sweetalert2';
+
 // Cargar las variables de entorno desde el archivo .env
 dotenv.config();
 
@@ -10,15 +13,14 @@ export default defineConfig({
   plugins: [react()],
   base: '/', // Ajusta esta ruta según sea necesario
   build: {
+
     manifest: false,
-   
   },
   server: {
     proxy: {
       '/api': {
-        target: 'https://caruso-prueba-back-1.onrender.com', 
+        target: 'https://caruso-prueba-back-1.onrender.com', // 28-06 
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
       },
     },
   },
