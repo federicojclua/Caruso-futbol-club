19382bcc6ee2a404aebae3ac25b040c65f5f2247
Maria Marcoux <mariadelcarmenmarcoux@gmail.com>
2024-06-29 20:48:46 -0300

    correcion vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import * as dotenv from 'dotenv';
+import dotenv from 'dotenv';
+
 
 // Cargar las variables de entorno desde el archivo .env
 dotenv.config();
