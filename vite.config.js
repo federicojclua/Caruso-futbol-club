d9c79e7cd5a8d19f87c07df9d9532f0336ed48c8
Maria Marcoux <mariadelcarmenmarcoux@gmail.com>
2024-06-29 21:42:34 -0300

    prueba deploy 6

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('vite');
 const react = require('@vitejs/plugin-react');
-const dotenv = require('dotenv');
+
 
 // Cargar las variables de entorno desde el archivo .env
 dotenv.config();
