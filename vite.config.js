1c0724b0e9ec2147c8874636f78ca8bb76149fa6
Maria Marcoux <mariadelcarmenmarcoux@gmail.com>
2024-06-29 21:55:47 -0300

    prueba deploy

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-const { defineConfig } = require('vite');
-const react = require('@vitejs/plugin-react');
-const dotenv = require('dotenv');
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 
 // Cargar las variables de entorno desde el archivo .env
-
 dotenv.config();
 
-module.exports = defineConfig({
+export default defineConfig({
   plugins: [react()],
   base: '/', // Ajusta esta ruta según sea necesario
   build: {
