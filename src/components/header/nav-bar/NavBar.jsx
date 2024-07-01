fa2782ce1972640b8e7bbb600a3d616553fe7ba8
Maru <mariadelcarmenmarcoux@gmail.com>
2024-06-30 23:15:54 -0300

    correcion navbar

--- src/components/header/nav-bar/NavBar.jsx
+++ src/components/header/nav-bar/NavBar.jsx
@@ -1,23 +1,34 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import "./NavBar.css";
 import Container from 'react-bootstrap/Container';
-import AuthContext from '../../context/AuthProvider'; // Importar AuthContext
+import AuthContext from '../../context/AuthProvider';
 import navLogo from '../../../assets/img/carpeta/cfc-logo.png';
 import userImagePlaceholder from '../../../assets/img/carpeta/IMAGEN-LOGO.png';
 
 const NavBar = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [userName, setUserName] = useState('');
+  const [userImage, setUserImage] = useState('');
   const navigate = useNavigate();
-  const { isLoggedIn, user, logout } = useContext(AuthContext); // Utilizar AuthContext
+  const { isLoggedIn, user, logout } = useContext(AuthContext);
+
+  const handleLogin = () => {
+    // Aquí simulas el proceso de inicio de sesión exitoso
+    setIsAuthenticated(true);
+    setUserName('Nombre de Usuario');
+    setUserImage('URL de la imagen del usuario');
+  };
 
   const handleLogout = () => {
+    // Aquí simulas el proceso de cierre de sesión
     logout();
-    navigate('/');
+    navigate('/')
   };
 
   const handleProtectedClick = (path) => {
-    if (isLoggedIn) {
+    if (isAuthenticated) {
       navigate(path);
     } else {
       navigate('/Login');
@@ -33,36 +44,36 @@ const NavBar = () => {
           </Link>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-            {user && user.nombre}
             <Nav className="me-auto">
               <Nav.Link as={Link} to="/Ecommerce">Nuestra Tienda</Nav.Link>
               <Nav.Link as={Link} to="/Location">Nuestros Complejos</Nav.Link>
               <NavDropdown title="Reservas" id="collasible-nav-dropdown">
                 <NavDropdown.Item onClick={() => handleProtectedClick('/principal')}>Reservar</NavDropdown.Item>
-                {isLoggedIn && (
+                {isAuthenticated && (
                   <NavDropdown.Item as={Link} to="/mis-reservas">Mis Reservas</NavDropdown.Item>
                 )}
               </NavDropdown>
-              {isLoggedIn && user && user.email === 'admin4@admin4.com' && (
+              {isAuthenticated && userName === 'admin4@admin4.com' && (
                 <Nav.Link as={Link} to="/admin">Administrador</Nav.Link>
               )}
             </Nav>
             <Nav>
-              {isLoggedIn && user ? ( // Mostrar nombre de usuario si está autenticado
-                <NavDropdown title={user.name || user.email} id="collasible-nav-dropdown">
+              {user ? (
+                <NavDropdown title={userName} id="collasible-nav-dropdown">
                   <div className="user-info">
-                    <img src={user.image || userImagePlaceholder} alt="User Avatar" className="user-avatar" />
-                    <span className="user-name">{user.name || user.email}</span>
+                    <img src={userImage || userImagePlaceholder} alt="User Avatar" className="user-avatar" />
+                    <span className="user-name">{userName}</span>
                   </div>
+                  <NavDropdown.Item as={Link} to="/perfil">Mi Perfil</NavDropdown.Item>
                   <NavDropdown.Item onClick={handleLogout}>Cerrar Sesión</NavDropdown.Item>
                 </NavDropdown>
               ) : (
                 <NavDropdown title="Iniciar Sesión" id="collasible-nav-dropdown">
                   <NavDropdown.Item as={Link} to="/Login">Inicia Sesión</NavDropdown.Item>
                   <NavDropdown.Item as={Link} to="/record">Regístrate</NavDropdown.Item>
+                  
                 </NavDropdown>
               )}
-
             </Nav>
           </Navbar.Collapse>
         </Container>
